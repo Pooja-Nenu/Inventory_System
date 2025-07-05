4ce7db988127b5950ee846758a449e9af2c04720
krishvatech <info@krishvatech.com>
2025-07-05 10:12:16 +0530

    Pooja 5-July Setup_Project

---
+++ footer.php
@@ -0,0 +1,114 @@
+<div class="copyright-footer d-flex align-items-center justify-content-between border-top bg-white gap-3 flex-wrap">
+				<p class="fs-13 text-gray-9 mb-0">2014 - 2025 &copy; DreamsPOS. All Right Reserved</p>
+				<p>Designed & Developed By <a href="javascript:void(0);" class="link-primary">Dreams</a></p>
+			</div>
+		</div>
+
+	</div>
+	<!-- /Main Wrapper -->
+
+	<!-- Add Stock -->
+	<div class="modal fade" id="add-stock">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<div class="page-title">
+						<h4>Add Stock</h4>
+					</div>
+					<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>					
+				<form action="https://dreamspos.dreamstechnologies.com/html/template/index.html">
+					<div class="modal-body">
+						<div class="row">
+							<div class="col-lg-12">
+								<div class="mb-3">
+									<label class="form-label">Warehouse <span class="text-danger ms-1">*</span></label>
+									<select class="select">
+										<option>Select</option>
+										<option>Lobar Handy</option>
+										<option>Quaint Warehouse</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-lg-12">
+								<div class="mb-3">
+									<label class="form-label">Store <span class="text-danger ms-1">*</span></label>
+									<select class="select">
+										<option>Select</option>
+										<option>Selosy</option>
+										<option>Logerro</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-lg-12">
+								<div class="mb-3">
+									<label class="form-label">Responsible Person <span class="text-danger ms-1">*</span></label>
+									<select class="select">
+										<option>Select</option>
+										<option>Steven</option>
+										<option>Gravely</option>
+									</select>
+								</div>
+							</div>
+							<div class="col-lg-12">
+								<div class="search-form mb-0">
+									<label class="form-label">Product <span class="text-danger ms-1">*</span></label>
+									<input type="text" class="form-control" placeholder="Select Product">
+									<i data-feather="search" class="feather-search"></i>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-md btn-dark me-2" data-bs-dismiss="modal">Cancel</button>
+						<button type="submit" class="btn btn-md btn-primary">Add Stock</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- /Add Stock -->
+
+	<!-- jQuery -->
+	<script src="assets/js/jquery-3.7.1.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Feather Icon JS -->
+	<script src="assets/js/feather.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Slimscroll JS -->
+	<script src="assets/js/jquery.slimscroll.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Bootstrap Core JS -->
+	<script src="assets/js/bootstrap.bundle.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- ApexChart JS -->
+	<script src="assets/plugins/apexchart/apexcharts.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+	<script src="assets/plugins/apexchart/chart-data.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Chart JS -->
+	<script src="assets/plugins/chartjs/chart.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+	<script src="assets/plugins/chartjs/chart-data.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Daterangepikcer JS -->
+	<script src="assets/js/moment.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+	<script src="assets/plugins/daterangepicker/daterangepicker.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Select2 JS -->
+	<script src="assets/plugins/select2/js/select2.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Color Picker JS -->
+	<script src="assets/plugins/%40simonwep/pickr/pickr.es5.min.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	<!-- Custom JS -->
+	<script src="assets/js/theme-colorpicker.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+	<script src="assets/js/script.js" type="1c62e688c711ce5a2f9ca272-text/javascript"></script>
+
+	
+<script src="https://dreamspos.dreamstechnologies.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="1c62e688c711ce5a2f9ca272-|49" defer></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"95753ad74ac17937","version":"2025.6.2","serverTiming":{"name":{"cfExtPri":true,"cfEdge":true,"cfOrigin":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"token":"3ca157e612a14eccbb30cf6db6691c29","b":1}' crossorigin="anonymous"></script>
+</body>
+
+
+<!-- Mirrored from dreamspos.dreamstechnologies.com/html/template/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 29 Jun 2025 12:00:02 GMT -->
+</html>
